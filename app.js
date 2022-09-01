d30d990f0631ec73c05fba777522d0b633205d99
CyrillaE <cyr.equi@gmail.com>
2022-09-01 19:28:45 +0100

    App JS

---
+++ app.js
@@ -0,0 +1,12 @@
+// Allowed the hamburger to spread in circle when pressed
+
+const hamburger = document.querySelector('.hamburger');
+const navlinks = document.querySelector('.nav-links');
+const links = document.querySelector('.nav-links li');
+
+hamburger.addEventListener('click', () =>{
+    navlinks.classList.toggle("open");
+    links.forEach(link =>{
+        link.classList.toggle("fade");
+    });
+});
